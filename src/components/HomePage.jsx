db176f209db51249b81b05a856207015765edfbf
Tafreed Ahmad <107949435+TafreedAhmad@users.noreply.github.com>
2024-03-03 14:59:54 +0500

    first commit

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,74 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+export default function HomePage(props) {
+    const { setAudioStream, setFile } = props
+
+    const [recordingStatus, setRecordingStatus] = useState('inactive')
+    const [audioChunks, setAudioChunks] = useState([])
+    const [duration, setDuration] = useEffect(0)
+
+    const mediaRecorder = useRef(null)
+    const mimeType = 'audio/webm'
+
+    async function startRecording() {
+        let tempStream
+
+        console.log('Start Recording')
+
+        try {
+            const streamData = navigator.mediaDevices.getUserMedia({
+                audio: true,
+                vidio: false
+            })
+            tempStream = streamData
+        } catch (error) {
+            console.log(error.message)
+            return
+        }
+
+        const media = new MediaRecorder(tempStream, { type: mimeType })
+
+        mediaRecorder.current = media
+        mediaRecorder.current.start()
+
+        let localAudioChunks = []
+        mediaRecorder.current.ondataavailable = (event) => {
+            if (typeof event.data === 'undefined') {
+                return;
+            }
+            if (typeof event.data === 0) {
+                return;
+            }
+            localAudioChunks.push(event.data)
+        }
+        setAudioChunks(localAudioChunks)
+    }
+
+    async 
+
+    return (
+        <main className='flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5 justify-center text-center'>
+            <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>
+                Free<span className='text-blue-400 bold'>Scribe</span>
+
+            </h1>
+            <h3 className='font-medium md:text-lg'>
+                Record <span className='text-blue-400'>&rarr;</span>
+                Transcribe<span className='text-blue-400'>&rarr;</span>
+                Translate
+            </h3>
+            <button className='flex items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4 specialBtn px-4 py-2 rounded-xl'>
+                <p className='text-blue-400'>Record</p>
+                <i className="fa-solid fa-microphone"></i>
+            </button>
+            <p className='text-base'>Or
+                <label className='text-blue-400 cursor-pointer hover:text-blue-600 duration-200'> upload
+                    <input className='hidden' type='file' accept='.mp3, .wave' onChange={(e) => {
+                        const tempFile = e.target.files[0]
+                        setFile(tempFile)
+                        if (!tempFile) return
+                    }} />
+                </label> a mp3 file</p>
+        </main>
+    )
+}
